371334f6baf2218e61c7d4e21c6cacc416012759
deepakyadav-02 <deepakyadav101002@gmail.com>
2024-02-04 02:32:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("yesBtn").addEventListener("click", function () {
+  window.location.href = "congrats.html";
+});
+
+document.getElementById("noBtn").addEventListener("mouseover", function () {
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+  const randomX = Math.floor(Math.random() * (viewportWidth - 150)); // Adjusted to keep button within viewport
+  const randomY = Math.floor(Math.random() * (viewportHeight - 50)); // Adjusted to keep button within viewport
+  const noButton = document.getElementById("noBtn");
+  noButton.style.position = "absolute";
+  noButton.style.left = randomX + "px";
+  noButton.style.top = randomY + "px";
+});
+
+document.getElementById("noBtn").addEventListener("click", function () {
+  this.innerHTML = "click yes";
+});
